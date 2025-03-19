8d1f1a7a4d534935fd140ffa5eefce6d47dd460b
Andrei Birdu Grecul <abirdu@ayesa.com>
2025-03-19 14:41:20 +0100

    Segundo commit

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from datetime import datetime
 
 app = Flask(__name__)
+   
 
 @app.route('/')
 def home():
@@ -9,6 +12,18 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/register' , methods = ['GET','POST'])
+def register():
+    print(request)
+    if request.method =='POST':
+        username = request.form['username']
+        password = request.form['password'] 
+        return f"Nombre de usuario: {username}, contraseñe {password}"       
+
+
+    return render_template('register.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
